524b038ec9271bdc230060ab9b1dde09c8344f23
FriedDust <fried.dust@gmail.com>
2016-09-05 18:45:37 +0545

    Cors Filter

--- src/main/java/com/frieddust/ramailodemo/JaxRsActivator.java
+++ src/main/java/com/frieddust/ramailodemo/JaxRsActivator.java
@@ -1,12 +1,7 @@
 package com.frieddust.ramailodemo;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-
-import com.ramailo.CorsFilter;
 
 /**
  * 
@@ -15,15 +10,4 @@ import com.ramailo.CorsFilter;
  */
 @ApplicationPath("/api/")
 public class JaxRsActivator extends Application {
-
-	private Set<Object> singletons = new HashSet<>();
-
-	public JaxRsActivator() {
-		singletons.add(new CorsFilter());
-	}
-
-	@Override
-	public Set<Object> getSingletons() {
-		return singletons;
-	}
 }
