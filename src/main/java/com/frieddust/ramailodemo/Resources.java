cd66abb94b0f0aa2950cf64498b22df84abc23f3
FriedDust <fried.dust@gmail.com>
2016-09-05 09:46:06 +0545

    Produce ClassFinder with package info

--- src/main/java/com/frieddust/ramailodemo/Resources.java
+++ src/main/java/com/frieddust/ramailodemo/Resources.java
@@ -4,6 +4,8 @@ import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import com.ramailo.util.ClassFinder;
+
 /**
  * 
  * @author Kailash Bijayananda <fried.dust@gmail.com>
@@ -14,4 +16,9 @@ public class Resources {
 	@Produces
 	@PersistenceContext(unitName = "primary")
 	private EntityManager em;
+	
+	@Produces
+	public ClassFinder classFinder() {
+		return new ClassFinder("com.frieddust.ramailodemo.entity");
+	}
 }
