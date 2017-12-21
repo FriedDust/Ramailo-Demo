1542e5dffc2d6fc6a4a3e615a13bbfe85df201e9
Fried Dust <fried.dust@gmail.com>
2017-12-21 13:35:08 +0545

    Test custom action invocation

--- src/main/java/com/frieddust/ramailodemo/entity/action/CustomerActions.java
+++ src/main/java/com/frieddust/ramailodemo/entity/action/CustomerActions.java
@@ -15,28 +15,21 @@ public class CustomerActions extends BaseActions<Customer> {
 	@Override
 	public void onBeforeSave() {
 		super.onBeforeSave();
-
-		System.out.println("Before saving.");
 	}
 
 	@Override
 	public void onSave() {
 		super.onSave();
-		System.out.println("After save.");
 	}
 
 	@Override
 	public void onBeforeDelete() {
 		super.onBeforeDelete();
-
-		System.out.println("Before delete.");
 	}
 
 	@Override
 	public void onDelete() {
 		super.onDelete();
-
-		System.out.println("After delete.");
 	}
 
 	@RamailoAction(label = "Greet")
